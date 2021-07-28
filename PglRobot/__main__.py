8525f75be8ea36fb49b7a7bfa87d5e4310167781
Delux <75972578+angad005@users.noreply.github.com>
2021-07-28 13:59:52 +0530

    Update __main__.py

--- PglRobot/__main__.py
+++ PglRobot/__main__.py
@@ -82,7 +82,7 @@ Uptime - {}!
 """
 
 HELP_STRINGS = """
-Hey there! My name is *{}*.
+Hey there! My name is *{}*
 I'm a Heroine For Fun and help admins to manage their groups! Have a look at the following for an idea of some of \
 the things I can help you with.
 *Main* commands available:
